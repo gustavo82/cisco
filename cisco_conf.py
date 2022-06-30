2a4b1fd310b76c2bee8431aa0905e5c0ce9efbf8
gustavo82 <gustavo_mello_@hotmail.com>
2022-06-29 21:56:01 -0300

    Update cisco_conf.py

--- cisco_conf.py
+++ cisco_conf.py
@@ -4,8 +4,9 @@ from platform import mac_ver
 from netmiko import ConnectHandler
 from cisco import Switch
 
-def __init__(self):
-    self.connect = ConnectHandler(**Switch)
+#def __init__(self):
+#    self.connect = ConnectHandler(**Switch)
+   
 
 def switch():
     IP = input("Qual Switch deseja configurar: ")
@@ -18,12 +19,12 @@ def switch():
     'username':usuario,
     'password':senha,
     }
-
+    global connect
     connect = ConnectHandler(**Switch)
     output = connect.find_prompt()
 
 menu = "1"
-while (menu != '4'):
+while (menu <= "6"):
     menu = input("Deseja opção desejada: \n 1) V-lan de VOZ \n 2) V-lan de Dados \n 3) Verificar V-lans \n 4) Buscar porta \n 5) limpar porta \n 6) Sair: ")
 
     if menu == "1": # VLAN DE VOZ
@@ -36,7 +37,7 @@ while (menu != '4'):
 
         ]
 
-        output = self.connect.send_config_set(alter_vlan)
+        output = connect.send_config_set(alter_vlan)
         print(output)
        
     if menu == "2": #VLAN DADOS
@@ -48,36 +49,34 @@ while (menu != '4'):
         'switch access vlan '+dados,
         ]
 
-        output = self.connect.send_config_set(alter_vlan)
+        output = connect.send_config_set(alter_vlan)
         print(output)
        
 
     if menu == "3": #MOSTRAR VLAN
         switch()
-        output = self.connect.send_command("terminal length 0")
-        output = self.connect.send_command("show vlan")
+        output = connect.send_command("terminal length 0")
+        output = connect.send_command("show vlan")
         print(output)
        
    
     if menu == "4": #Buscar porta
         mac = input("Digite o MAC:")
-        output = self.connect.send_command("terminal length 0")
-        output = self.connect.send_command("show mac address-table addres "+mac)
+        output = connect.send_command("terminal length 0")
+        output = connect.send_command("show mac address-table addres "+mac)
         print(output)
        
 
     if menu == "5": #limpar porta
-        output = self.connect.find_prompt()
-        output = self.connect.send_command("terminal length 0")
-        alter_vlan = [
-        'interface '+porta,
-        ' sh',
-        'no shu',
-        ]
-        output = self.connect.send_config_set(alter_vlan)
+        mac = input("Digite o MAC:")
+        switch()
+        output = connect.find_prompt()
+        output = connect.send_command("terminal length 0")
+        limpa_porta = ['clear port-security all address' +mac]
+        output = connect.send_config_set(limpa_porta)
         print(output)
     else:
     #if menu == "6": #sair
         print("Saindo")
-        output = self.connect.disconnect()
+        #connect.disconnect()
         break
